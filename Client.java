997cf300a5b99b6ff8df53e7e0c5cbd5255927c5
Tendry <tendrirazafimiandrisoa@gmail.com>
2022-11-18 20:37:27 +0300

    change message

--- Client.java
+++ Client.java
@@ -6,7 +6,7 @@ public class Client {
         try {
             Socket socket = new Socket("localhost", 8090);
             OutputStream out = socket.getOutputStream();
-            String msg = "Tendry Ny Avo";
+            String msg = "Aina avotra";
             out.write(msg.getBytes());
             out.close();
             socket.close();
